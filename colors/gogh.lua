1597500f47b9f3ee05fb4d0d2394f6d9d5e9f60b
hugoocoto <88594185+hugoocoto@users.noreply.github.com>
2026-09-10 09:51:14 +0000

    Regenerate colorscheme files [skip ci]

--- colors/gogh.lua
+++ colors/gogh.lua
@@ -6,10 +6,10 @@
 -- be set after setup() runs, not before.
 require("base16-colorscheme").setup({
   base00 = "#1d2021",
-  base01 = "#928374",
+  base01 = "#282828",
   base02 = "#141617",
   base03 = "#928374",
-  base04 = "#d4be98",
+  base04 = "#a89984",
   base05 = "#d4be98",
   base06 = "#ddc7a1",
   base07 = "#ffffff",
@@ -20,7 +20,7 @@ require("base16-colorscheme").setup({
   base0C = "#89b482",
   base0D = "#7daea3",
   base0E = "#d3869b",
-  base0F = "#e78a4e",
+  base0F = "#ad683a",
 })
 
 vim.g.colors_name = "gogh"
